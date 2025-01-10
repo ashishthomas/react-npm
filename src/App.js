77a9bc39f226f4d9ca2f90ddd62f918131830374
ashish <ashishthomas1995@gmail.com>
2025-01-10 16:09:37 +0530

    created a new files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> Rendering the React Page </header>
+    </div>
+  );
+}
+
+export default App;
